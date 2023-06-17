3acfc6833b12f2dafb5bc74a052d1f8decaba726
moodham <60978858+moodham@users.noreply.github.com>
2023-06-17 08:27:56 -0700

    3
    33

--- mypr/urls.py
+++ mypr/urls.py
@@ -22,9 +22,11 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("", include('blogs.urls')),
     path('admin/', admin.site.urls),
+    
+
     path('tinymce/', include('tinymce.urls')),
+    path("", include('blogs.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
